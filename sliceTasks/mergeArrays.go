505abe0a7a62f5516571490f7b538c3d5cbb92cb
Herman <ishi25252525@gmail.com>
2025-02-20 17:49:35 +0300

    ref: moved files to a new folder

--- sliceTasks/mergeArrays.go
+++ sliceTasks/mergeArrays.go
@@ -5,7 +5,11 @@ package tasks
 состоящий из элементов обоих массивов.
 */
 
-func mergeArr(arr1 []int, arr2 []int) []int {
-	result := append(arr1, arr2...)
+func merge(firstArr []int, secondArr []int) []int {
+
+	result := make([]int, len(firstArr)+len(secondArr))
+	copy(result, firstArr)
+	copy(result[len(firstArr):], secondArr)
+
 	return result
 }
